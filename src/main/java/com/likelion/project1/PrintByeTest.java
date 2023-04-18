e8f4074c3a4e0c86716118fb8eea9725811d3335
KOEYHNI <dlsgur1225@naver.com>
2023-04-18 23:02:42 +0900

    first commit

---
+++ src/main/java/com/likelion/project1/PrintByeTest.java
@@ -0,0 +1,8 @@
+package com.likelion.project1;
+
+public class PrintByeTest {
+    public static void main(String[] args) {
+        PrintBye printbye = new PrintBye();
+        printbye.print();
+    }
+}
---
+++ src/main/java/com/likelion/project1/PrintHelloTest.java
@@ -0,0 +1,8 @@
+package com.likelion.project1;
+
+public class PrintHelloTest {
+    public static void main(String[] args) {
+        PrintHello printhello = new PrintHello();
+        printhello.print();
+    }
+}
